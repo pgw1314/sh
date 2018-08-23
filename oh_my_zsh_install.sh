3b2d51420161b46ff7ad9006c6f9a154b500b618
XiaoWei <pgw1314@gmail.com>
2018-08-23 18:01:37 +0800

    将oh_my_zsh_install.sh中的插件安装功能移动到oh_my_zsh_plugin_install.sh中！

--- oh_my_zsh_install.sh
+++ oh_my_zsh_install.sh
@@ -14,34 +14,6 @@
 . ./funs/m_print.sh
 . ./funs/m_utils.sh
 
-autojump_plugin_install(){
-    autojump_path=/tmp/autojump
-    git clone git://github.com/joelthelion/autojump.git $autojump_path
-    $autojump_path/install.py
-    if [[ $? != 0 ]]; then
-        print_r "错误：autojump插件安装失败!"
-        exit
-    fi
-    rm -rf $autojump_path
-}
-
-
-syntax_highlighting_plugin_install(){
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    if [[ $? != 0 ]]; then
-        print_r "错误：syntax-highlighting插件安装失败!"
-        exit
-    fi
-}
-
-autosuggestions_plugin_install(){
-    git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-    if [[ $? != 0 ]]; then
-        print_r "错误：autosuggestions插件安装失败!"
-        exit
-    fi
-}
-
 
 
 mac_install(){
@@ -82,24 +54,6 @@ elif [[ $os_type == 2 ]]; then
 fi
 print_g "oh-my-zsh安装完成！"
 
-#--------------------------插件安装-------------------------------------------
-print_y "开始安装autojump插件...."
-autojump_plugin_install
-print_g "autojump插件安装完成！"
-
-print_y "开始安装syntax-highlighting插件...."
-syntax_highlighting_plugin_install
-print_g "syntax-highlighting插件安装完成！"
-
-print_y "开始安装autosuggestions插件...."
-autosuggestions_plugin_install
-print_g "autosuggestions插件安装完成！"
-
-#--------------------------配置-------------------------------------------
-print_y "开始安装配置oh-my-zsh..."
-$(cat ./conf/zshrc  > ~/.zshrc )
-source ~/.zshrc
-print_g "oh-my-zsh配置完成！"
 
 
 
---
+++ oh_my_zsh_plugin_install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#############################################################
+#
+#功能： 该脚本是用来安装oh-my-zsh的插件
+#
+#版本：v1.0.1
+#
+#最后修改时间：2018年8月23日
+#
+#############################################################
+
+
+#引入脚本
+. ./funs/m_print.sh
+. ./funs/m_utils.sh
+
+autojump_plugin_install(){
+    autojump_path=/tmp/autojump
+    git clone git://github.com/joelthelion/autojump.git $autojump_path
+    $autojump_path/install.py
+    if [[ $? != 0 ]]; then
+        print_r "错误：autojump插件安装失败!"
+        exit
+    fi
+    rm -rf $autojump_path
+}
+
+
+syntax_highlighting_plugin_install(){
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    if [[ $? != 0 ]]; then
+        print_r "错误：syntax-highlighting插件安装失败!"
+        exit
+    fi
+}
+
+autosuggestions_plugin_install(){
+    git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+    if [[ $? != 0 ]]; then
+        print_r "错误：autosuggestions插件安装失败!"
+        exit
+    fi
+}
+
+
+
+
+
+#判断是否安装zsh
+is_install=$(cat /etc/shells |grep zsh)
+if [[ -z $is_install ]]; then
+    print_r "请先运行oh_my_zsh_install，安装oh-my-zsh!"
+    exit
+fi
+
+#--------------------------插件安装-------------------------------------------
+print_y "开始安装autojump插件...."
+autojump_plugin_install
+print_g "autojump插件安装完成！"
+
+print_y "开始安装syntax-highlighting插件...."
+syntax_highlighting_plugin_install
+print_g "syntax-highlighting插件安装完成！"
+
+print_y "开始安装autosuggestions插件...."
+autosuggestions_plugin_install
+print_g "autosuggestions插件安装完成！"
+
+#--------------------------配置-------------------------------------------
+print_y "开始安装配置oh-my-zsh..."
+$(cat ./conf/zshrc  > ~/.zshrc )
+source ~/.zshrc
+print_g "oh-my-zsh配置完成！"
+
+
+
+
+
