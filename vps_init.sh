c8ee12805346a10b507e5af7bb5d8fc32080e6c5
XiaoWei <pgw1314@gmail.com>
2018-08-24 00:16:32 +0800

    添加提示信息

--- vps_init.sh
+++ vps_init.sh
@@ -12,65 +12,102 @@
 print_r(){
     echo -e "\033[31m $@ \033[0m"
 }
+print_g(){
+    echo -e "\033[32m $@ \033[0m"
+}
+print_y(){
+    echo -e "\033[33m $@ \033[0m"
+}
 
 Error="[错误]:"
+Info="[信息]："
 sh_path=~/sh
-install_sh_path=/tmp/install.sh
+# install_sh_path=/tmp/install.sh
 github_path=https://raw.githubusercontent.com/pgw1314/sh/master/install.sh
 #下载安装脚本
-curl -o $install_sh_path $github_path
+print_y "$Info 开始下载安装脚本..."
+yum -y install wget
+if [[ condition ]]; then
+    print_r "$Error wget安装失败！"
+    exit
+fi
+rm -rf ./install.sh
+wget $github_path
 if [[ $? != 0 ]]; then
     print_r "$Error 安装脚本下载失败！"
     exit
 fi
+print_g"$Info 安装脚本下载完成"
+
 
 #运行安装脚本
-$(sh $install_sh_path)
+#给安装脚本执行权限
+print_y "$Info 开始执行安装脚本..."
+chmod 755 ./install.sh
+./install.sh
 if [[ $? != 0 ]]; then
     print_r "$Error 安装脚本安装过程中出错了！"
     exit
 fi
+print_g"$Info 安装脚本执行完成"
+
 
 #安装基本软件
-$($sh_path/bash_install.sh)
+print_y "$Info 开始安装基础软件包..."
+$sh_path/base_install.sh
 if [[ $? != 0 ]]; then
     print_r "$Error 基本软件安装中出错了！"
     exit
 fi
+print_g"$Info 基本软件安装完成"
+
 
 #安装opencc
-$($sh_path/opencc_install.sh)
+print_y "$Info 开始安装opencc软件包..."
+$sh_path/opencc_install.sh
 if [[ $? != 0 ]]; then
     print_r "$Error Opencc安装中出错了！"
     exit
 fi
+print_g"$Info opencc软件包安装完成"
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/pgw1314/sh/master/install.sh)"
 
 #安装youtube_upload_install
-$($sh_path/youtube_upload_install.sh)
+print_y "$Info 开始安装youtube-upload软件包..."
+$sh_path/youtube_upload_install.sh
 if [[ $? != 0 ]]; then
     print_r "$Error youtube_upload_install安装中出错了！"
     exit
 fi
+print_g"$Info youtube-upload软件包安装完成"
 
 #aria2
-$($sh_path/aria2.sh)
+print_y "$Info 开始安装aria2软件包..."
+$sh_path/aria2.sh
 if [[ $? != 0 ]]; then
     print_r "$Error aria2安装中出错了！"
     exit
 fi
+print_g"$Info aria2软件包安装完成"
 
 #安装shadowsocksR
-$($sh_path/shadowsocksR.sh)
+print_y "$Info 开始安装shadowsocksR软件包..."
+$sh_path/shadowsocksR.sh
 if [[ $? != 0 ]]; then
     print_r "$Error shadowsocksR安装中出错了！"
     exit
 fi
+print_g"$Info shadowsocksR软件包安装完成"
+
+
 #安装oh_my_zsh_install
-$($sh_path/oh_my_zsh_install.sh)
+print_y "$Info 开始安装oh-my-zsh软件包..."
+$sh_path/oh_my_zsh_install.sh
 if [[ $? != 0 ]]; then
     print_r "$Error oh_my_zsh_install安装中出错了！"
     exit
 fi
+print_g"$Info oh-my-zsh软件包安装完成"
 
-rm -rf $install_sh_path
+# 删除install文件
+rm -rf ./install.sh
