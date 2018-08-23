58b05482a2e711058cbf49dae8c0b02dd1073056
XiaoWei <pgw1314@gmail.com>
2018-08-23 23:46:08 +0800

    修复install.sh运行权限问题！

--- vps_init.sh
+++ vps_init.sh
@@ -8,6 +8,11 @@
 #最后修改时间：2018年8月23日
 #
 #############################################################
+
+print_r(){
+    echo -e "\033[31m $@ \033[0m"
+}
+
 Error="[错误]:"
 sh_path=~/sh
 install_sh_path=/tmp/install.sh
@@ -15,28 +20,28 @@ github_path=https://raw.githubusercontent.com/pgw1314/sh/master/install.sh
 #下载安装脚本
 curl -o $install_sh_path $github_path
 if [[ $? != 0 ]]; then
-    echo "$Error 安装脚本下载失败！"
+    print_r "$Error 安装脚本下载失败！"
     exit
 fi
 
 #运行安装脚本
-$($install_sh_path)
+$(sh $install_sh_path)
 if [[ $? != 0 ]]; then
-    echo "$Error 安装脚本安装过程中出错了！"
+    print_r "$Error 安装脚本安装过程中出错了！"
     exit
 fi
 
 #安装基本软件
 $($sh_path/bash_install.sh)
 if [[ $? != 0 ]]; then
-    echo "$Error 基本软件安装中出错了！"
+    print_r "$Error 基本软件安装中出错了！"
     exit
 fi
 
 #安装opencc
 $($sh_path/opencc_install.sh)
 if [[ $? != 0 ]]; then
-    echo "$Error Opencc安装中出错了！"
+    print_r "$Error Opencc安装中出错了！"
     exit
 fi
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/pgw1314/sh/master/install.sh)"
@@ -44,27 +49,27 @@ fi
 #安装youtube_upload_install
 $($sh_path/youtube_upload_install.sh)
 if [[ $? != 0 ]]; then
-    echo "$Error youtube_upload_install安装中出错了！"
+    print_r "$Error youtube_upload_install安装中出错了！"
     exit
 fi
 
 #aria2
 $($sh_path/aria2.sh)
 if [[ $? != 0 ]]; then
-    echo "$Error aria2安装中出错了！"
+    print_r "$Error aria2安装中出错了！"
     exit
 fi
 
 #安装shadowsocksR
 $($sh_path/shadowsocksR.sh)
 if [[ $? != 0 ]]; then
-    echo "$Error shadowsocksR安装中出错了！"
+    print_r "$Error shadowsocksR安装中出错了！"
     exit
 fi
 #安装oh_my_zsh_install
 $($sh_path/oh_my_zsh_install.sh)
 if [[ $? != 0 ]]; then
-    echo "$Error oh_my_zsh_install安装中出错了！"
+    print_r "$Error oh_my_zsh_install安装中出错了！"
     exit
 fi
 
