afe555f354f40f1f8ec5f4e51dec7441cb68687a
XiaoWei <pgw1314@gmail.com>
2018-08-24 13:58:38 +0800

    修改install.sh提示信息！

--- install.sh
+++ install.sh
@@ -18,10 +18,12 @@ print_g(){
 print_b(){
     echo -e "\033[34m $@ \033[0m"
 }
-print_y(){
+print_r(){
     echo -e "\033[33m $@ \033[0m"
 }
 
+Error="[错误]:"
+Info="[信息]："
 
 shell_path=/usr/local/shell
 bin_path=/usr/local/bin
@@ -54,7 +56,7 @@ replase_file_content(){
             #echo "Linux 操作系统"
             sed -i "s/$pattern/$rep_con/g" $file_path
         else
-            print_r "错误：你的系统不支持！！"
+            print_r  $Error "错误：你的系统不支持！！"
             exit
         fi
 
@@ -70,7 +72,7 @@ read_dir(){
     ls $my_path | while read file; do
     	file_path=$my_path"/"$file
 
-    	#print_y "开始复制："$file_path
+    	#print_r $Info  "开始复制："$file_path
     	temp_file=$temp_path"/"$file
     	if [[ -d $file_path ]]; then
     		cp -r $file_path $temp_file
@@ -90,13 +92,13 @@ read_dir(){
             fi
     		file_type=${file_path##*.}
     		if [[ $file_type == "sh" ]]; then
-    			#print_y "开始配置："$file_path
+    			#print_r $Info  "开始配置："$file_path
     			#替换文件路径
     			if [[ $2 == "rep"  ]]; then
                     # echo "-------------------------------------"
-                    # print_g "file_path=$file_path"
-                    # print_g "temp_file=$temp_file"
-                    # print_g "file=$file"
+                    # print_g $Info "file_path=$file_path"
+                    # print_g $Info "temp_file=$temp_file"
+                    # print_g $Info "file=$file"
                     # echo "-------------------------------------"
                     #复制脚本
                     cp $file_path $temp_file
@@ -109,7 +111,7 @@ read_dir(){
     			fi
     			#配置环境变量
     			if [[ $2 == "env"  ]]; then
-    				print_y "开始配置环境变量：$file_path"
+    				print_r $Info  "开始配置环境变量：$file_path"
     				old_file_path=$file_path
                     #获取到文件名和路径
                     #file_path=${file_path%/*}
@@ -120,7 +122,7 @@ read_dir(){
 
     				ln -s $old_file_path $bin_path"/"$file_name
                     if [[ $? == 0 ]]; then
-                        print_g "环境变量配置完成：$bin_path/$file_name"
+                        print_g $Info "环境变量配置完成：$bin_path/$file_name"
                         else
                         
                         return 127
@@ -136,64 +138,73 @@ read_dir(){
 }
 
 #-----------------------------主程序开始------------------------------------------
+
+#------------------------------开始部署脚本---------------------------------------
 #从GitHub上获取程序
 sh_path=~/sh
 github_path="https://github.com/pgw1314/sh.git"
 if [[ ! -d $sh_path ]]; then
     yum -y install git
     if [[ $? != 0 ]]; then
-        print_r "错误：安装git失败"
+        print_r $Error "安装git失败"
         exit
     fi
+    rm -rf $sh_path
     git clone $github_path  $sh_path
     if [[ $? != 0 ]]; then
-        print_r "错误：从GitHub拉取脚本失败！"
+        print_r $Error "从GitHub拉取脚本失败！"
         exit
     fi
 fi
 
-# done
 #将所有代码拷贝到/usr/local/shell
 rm -rf $temp_path
-#print_y "请输入管理员密码！"
+#print_r $Info  "请输入管理员密码！"
 
 mkdir -p $temp_path
 if [[ $? != 0 ]]; then
-	print_r "临时文件夹：$temp_path创建错误！"
+	print_r $Error "临时文件夹：$temp_path创建错误！"
     exit
 fi
 #替换引用文件路径
-print_y "开始修复脚本中的引用路径..."
+print_r $Info  "开始修复脚本中的引用路径..."
 read_dir $sh_path rep
-print_g "引用路径修复成功！"
+print_g $Info "引用路径修复成功！"
 
-print_y "开始复制脚本文件..."
+print_r $Info  "开始复制脚本文件..."
 
 sudo rm -rf $shell_path
 if [[ $? != 0 ]]; then
-    print_r "获取Root权限失败！！"
+    print_r $Error "获取Root权限失败！！"
     exit
 fi
 sudo cp -r $temp_path /usr/local/
 if [[ $? == 0 ]]; then
-    print_g "脚本文件复制完成！！"
+    print_g $Info "脚本文件复制完成！！"
 else
-    print_r "安装错误：安装失败！！"
+    print_r $Error "脚本复制失败！！"
     exit
 fi
 
 sudo chmod -R 755 $shell_path
 if [[ $? != 0 ]]; then
-    print_r "获取Root权限失败！！"
+    print_r $Error "获取Root权限失败！！"
     exit
 fi
 
-print_y "开始配置环境变量..."
+# print_r $Info  "开始配置环境变量..."
 read_dir $shell_path env
 if [[ $? != 0 ]]; then
-    print_r "错误：环境变量配置失败！！"
+    print_r $Error "环境变量配置失败！！"
     exit
 fi
 rm -rf $temp_path
-print_g "脚本安装完成！！"
+print_g $Info "脚本部署完成完成！！"
+
+
+
+#------------------------------开始安装软件---------------------------------------
+
+
+
 
