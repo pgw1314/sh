c6a4e4e9600c2d83a3a37036109a240caa8df634
root <root@vultr.guest>
2018-09-02 07:01:50 +0000

    修改Bt Tracker列表！

--- confsh/trackers-list-aria2.sh
+++ confsh/trackers-list-aria2.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 pkill aria2c
-list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [ -z "`grep "bt-tracker" /root/.aria2/aria2.conf`" ]; then
     sed -i '$a bt-tracker='${list} /root/.aria2/aria2.conf
     echo add......
@@ -10,5 +10,5 @@ else
     echo update......
 fi
 
-/usr/local/bin/aria2c --conf-path=/Users/xiaowei/.aria2/aria2.conf -D
+/usr/bin/aria2c --conf-path=/root/.aria2/aria2.conf -D
 
