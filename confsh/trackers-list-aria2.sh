56d659532a21f07181eef8847b9c0f816362daae
XiaoWei <pgw1314@gmail.com>
2018-09-02 15:17:41 +0800

    使用自己的bt_tracker.txt文件

--- confsh/trackers-list-aria2.sh
+++ confsh/trackers-list-aria2.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 pkill aria2c
-list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+list=`cat ../conf/bt_tracker.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [ -z "`grep "bt-tracker" /root/.aria2/aria2.conf`" ]; then
     sed -i '$a bt-tracker='${list} /root/.aria2/aria2.conf
     echo add......
